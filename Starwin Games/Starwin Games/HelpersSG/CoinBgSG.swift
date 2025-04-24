723dac49e7616f637d64e4ad794b57164d910f4e
Dias <dias.atudinov@gmail.com>
2025-04-25 00:35:25 +0400

    add helpers: Coins bg and device manager

--- Starwin Games/Starwin Games/HelpersSG/CoinBgSG.swift
+++ Starwin Games/Starwin Games/HelpersSG/CoinBgSG.swift
@@ -1,15 +1,23 @@
-//
-//  CoinBgSG.swift
-//  Starwin Games
-//
-//  Created by Dias Atudinov on 24.04.2025.
-//
-
 import SwiftUI
 
 struct CoinBgSG: View {
+    @StateObject var user = GEUser.shared
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image(.coinsViewBgSG)
+                .resizable()
+                .scaledToFit()
+            
+            Text("\(user.money)")
+                .font(.system(size: GEDeviceManager.shared.deviceType == .pad ? 40:20, weight: .black))
+                .foregroundStyle(.yellow)
+                .textCase(.uppercase)
+                .offset(x: GEDeviceManager.shared.deviceType == .pad ? 30:15, y: GEDeviceManager.shared.deviceType == .pad ? 16:8)
+            
+            
+            
+        }.frame(height: GEDeviceManager.shared.deviceType == .pad ? 126:63)
+        
     }
 }
 
