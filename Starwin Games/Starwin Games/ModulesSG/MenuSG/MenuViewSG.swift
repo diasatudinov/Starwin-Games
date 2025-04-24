ff259eaded6972266d4bbd5e1f39b7c0889ae90e
Dias <dias.atudinov@gmail.com>
2025-04-25 00:35:47 +0400

    Splash Screen and Menu View

--- Starwin Games/Starwin Games/ModulesSG/MenuSG/MenuViewSG.swift
+++ Starwin Games/Starwin Games/ModulesSG/MenuSG/MenuViewSG.swift
@@ -1,16 +1,134 @@
-//
-//  MenuViewSG.swift
-//  Starwin Games
-//
-//  Created by Dias Atudinov on 24.04.2025.
-//
-
 import SwiftUI
 
 struct MenuViewSG: View {
+    @State private var showGame = false
+    @State private var showShop = false
+    @State private var showAchievement = false
+    @State private var showMiniGames = false
+    @State private var showSettings = false
+    
+//    @StateObject var achievementVM = AchievementsViewModel()
+    @StateObject var settingsVM = SettingsViewModelSG()
+//    @StateObject var calendarVM = CalendarViewModel()
+//    @StateObject var shopVM = ShopViewModelGE()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            
+            Image(.logoSG)
+                .resizable()
+                .scaledToFit()
+                .frame(height: GEDeviceManager.shared.deviceType == .pad ? 300:150)
+                .padding(.bottom, GEDeviceManager.shared.deviceType == .pad ? 440:220)
+            
+            VStack(spacing: 0) {
+                HStack(alignment: .top) {
+                    CoinBgSG()
+                    
+                    Spacer()
+                    
+                    Button {
+                        showSettings = true
+                    } label: {
+                        Image(.settingsIconSG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: GEDeviceManager.shared.deviceType == .pad ? 140:70)
+                    }
+                }
+                
+                
+                Spacer()
+                Button {
+                    showGame = true
+                } label: {
+                    Image(.playIconSG)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: GEDeviceManager.shared.deviceType == .pad ? 240:180)
+                        .padding(.top, GEDeviceManager.shared.deviceType == .pad ? 180:90)
+                }
+                
+                Spacer()
+                ZStack {
+                    HStack {
+                        Button {
+                            showMiniGames = true
+                        } label: {
+                            Image(.miniGamesIconSG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: GEDeviceManager.shared.deviceType == .pad ? 140:90)
+                        }
+                    }
+                    
+                    HStack(alignment: .bottom) {
+                        
+                        Button {
+                            showShop = true
+                        } label: {
+                            Image(.shopIconSG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: GEDeviceManager.shared.deviceType == .pad ? 140:70)
+                        }
+                        Spacer()
+                        
+                        Button {
+                            showAchievement = true
+                        } label: {
+                            Image(.achivsIconSG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: GEDeviceManager.shared.deviceType == .pad ? 140:70)
+                        }
+                    }
+                }
+            }.padding()
+                .ignoresSafeArea(edges: .bottom)
+        }
+        .background(
+            ZStack {
+                Image(.menuBgSG)
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .scaledToFill()
+            }
+        )
+//                    .onAppear {
+//                        if settingsVM.musicEnabled {
+//                            GEMusicManager.shared.playBackgroundMusic()
+//                        }
+//                    }
+//                    .onChange(of: settingsVM.musicEnabled) { enabled in
+//                        if enabled {
+//                            GEMusicManager.shared.playBackgroundMusic()
+//                        } else {
+//                            GEMusicManager.shared.stopBackgroundMusic()
+//                        }
+//                    }
+        .fullScreenCover(isPresented: $showGame) {
+            //SelectGameView(shopVM: shopVM)
+        }
+        .fullScreenCover(isPresented: $showMiniGames) {
+           // CalendarView(viewModel: calendarVM)
+        }
+        .fullScreenCover(isPresented: $showAchievement) {
+           // AchievementsView(viewModel: achievementVM)
+        }
+        .fullScreenCover(isPresented: $showShop) {
+           // ShopViewGE(viewModel: shopVM)
+        }
+        .fullScreenCover(isPresented: $showSettings) {
+            SettingsViewSG(settingsVM: settingsVM)
+        }
+        
+        
+        
+        
     }
+    
 }
 
 #Preview {
--- Starwin Games/Starwin Games/ModulesSG/MenuSG/SplashScreenSG.swift
+++ Starwin Games/Starwin Games/ModulesSG/MenuSG/SplashScreenSG.swift
@@ -1,15 +1,84 @@
-//
-//  SplashScreenSG.swift
-//  Starwin Games
-//
-//  Created by Dias Atudinov on 24.04.2025.
-//
-
 import SwiftUI
 
 struct SplashScreenSG: View {
+    @State private var scale: CGFloat = 1.0
+    @State private var progress: CGFloat = 0.0
+    @State private var timer: Timer?
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image(.splashScreenBgSG)
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                
+                ZStack {
+                    Image(.logoSG)
+                        .resizable()
+                        .scaledToFit()
+                    
+                    
+                }
+                .frame(height: 150)
+                
+                Spacer()
+                
+                Image(.loadingTextSG)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 24)
+                    .scaleEffect(scale)
+                    .animation(
+                        Animation.easeInOut(duration: 0.8)
+                            .repeatForever(autoreverses: true),
+                        value: scale
+                    )
+                    .onAppear {
+                        scale = 0.8
+                    }
+                    .padding(.bottom, 15)
+                
+                ZStack {
+                   
+                    Image(.loaderInsideSG)
+                        .resizable()
+                        .scaledToFit()
+                        .colorMultiply(.gray)
+                    
+                    Image(.loaderInsideSG)
+                        .resizable()
+                        .scaledToFit()
+                        .mask(
+                            Rectangle()
+                                .frame(width: progress * 250)
+                                .padding(.trailing, (1 - progress) * 250)
+                        )
+                    Image(.loaderBorderSG)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 250)
+                }
+                .frame(width: 250)
+            }
+            
+            
+        }
+        .onAppear {
+            startTimer()
+        }
+    }
+    
+    func startTimer() {
+        timer?.invalidate()
+        progress = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 0.07, repeats: true) { timer in
+            if progress < 1 {
+                progress += 0.01
+            } else {
+                timer.invalidate()
+            }
+        }
     }
 }
 
