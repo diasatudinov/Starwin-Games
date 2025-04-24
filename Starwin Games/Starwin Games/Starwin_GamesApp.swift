625a39313d0f2578875d768d49c5bcc208fa998b
Dias <dias.atudinov@gmail.com>
2025-04-24 21:12:59 +0400

    app set up

---
+++ Starwin Games/Starwin Games/Starwin_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Starwin_GamesApp.swift
+//  Starwin Games
+//
+//  Created by Dias Atudinov on 24.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct Starwin_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
